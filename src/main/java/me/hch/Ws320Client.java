369930da2b6e5259dc1db4c65756137603f5958f
doodlecoge@163.com <accountbase@163.com>
2014-04-02 17:23:55 +0800

    add endpoint publisher and test client

---
+++ src/main/java/me/hch/Ws320Client.java
@@ -0,0 +1,36 @@
+package me.hch;
+
+import me.hch.service.GetHospInfoReq;
+import me.hch.service.GetHospInfoRsp;
+import me.hch.service.Ws320;
+import me.hch.service.Ws320ImplService;
+import me.hch.service_impl.Ws320Impl;
+
+import javax.xml.namespace.QName;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by huaiwang on 14-4-2.
+ */
+public class Ws320Client {
+    public static void main(String[] args) throws MalformedURLException {
+        String wsdlLoc = "http://127.0.0.1:8088/aaa?wsdl";
+        String ns = "http://service.hch.me/";
+        String serviceName = "CalculatorServiceImpl";
+
+        // do not use no arguments constructor
+        // pass wsdl location, namespace and service name manully
+        Ws320ImplService s = new Ws320ImplService(
+                new URL(wsdlLoc), new QName(ns, serviceName)
+        );
+
+        GetHospInfoReq req = new GetHospInfoReq();
+
+        Ws320 p = s.getWs320ImplPort();
+        GetHospInfoRsp hospInfo = p.getHospInfo(req);
+
+
+        System.out.println(hospInfo.getHospital().get(0).getHospName());
+    }
+}
--- src/main/java/me/hch/service_impl/Ws320Impl.java
+++ src/main/java/me/hch/service_impl/Ws320Impl.java
@@ -1,14 +1,26 @@
 package me.hch.service_impl;
 
 import me.hch.service.*;
+import me.hch.service.Hospital;
+
+import javax.jws.WebService;
 
 /**
  * Created by hch on 2014/3/30.
  */
+@WebService(
+        serviceName = "CalculatorServiceImpl",
+        endpointInterface = "me.hch.service.Ws320",
+        targetNamespace = "http://service.hch.me/")
 public class Ws320Impl implements Ws320 {
     @Override
     public GetHospInfoRsp getHospInfo(GetHospInfoReq parameters) {
-        return null;
+        Hospital hospital = new Hospital();
+        hospital.setHospName("SDFY");
+
+        GetHospInfoRsp rsp = new GetHospInfoRsp();
+        rsp.getHospital().add(hospital);
+        return rsp;
     }
 
     @Override
