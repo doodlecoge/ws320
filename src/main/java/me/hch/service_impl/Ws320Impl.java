ac1fe1b62e08efc6ae0df1fc2a3fdca64c18cc17
huaichao wang <hchwang001@gmail.com>
2014-04-02 08:06:19 +0800

    add service impl: Ws320Impl

---
+++ src/main/java/me/hch/service_impl/Ws320Impl.java
@@ -0,0 +1,23 @@
+package me.hch.service_impl;
+
+import me.hch.service.*;
+
+/**
+ * Created by hch on 2014/3/30.
+ */
+public class Ws320Impl implements Ws320 {
+    @Override
+    public GetHospInfoRsp getHospInfo(GetHospInfoReq parameters) {
+        return null;
+    }
+
+    @Override
+    public GetRegInfoRsp getRegInfo(GetRegInfoReq parameters) {
+        return null;
+    }
+
+    @Override
+    public GetRegPoolRsp getRegPool(GetRegPoolReq parameters) {
+        return null;
+    }
+}
