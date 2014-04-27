c9e2f0850ad70b04a15901fee9597e5131e87788
huaichao wang <hchwang001@gmail.com>
2014-04-27 09:55:23 +0800

    the id name mapper

--- src/main/java/me/hch/model/NameIdMapper.java
+++ src/main/java/me/hch/model/NameIdMapper.java
@@ -1,21 +1,84 @@
 package me.hch.model;
 
+import me.hch.Ws320Exception;
+
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by hch on 2014/4/26.
  */
 public class NameIdMapper {
-    public String id;
 
-    public class Hospital extends NameIdMapper {
-        private Map<String, Department> departments;
+
+    public class HospitalMapper {
+        public String id;
+        private Map<String, DepartmentMapper> departments =
+                new HashMap<String, DepartmentMapper>();
     }
 
-    public class Department extends NameIdMapper {
-        private Map<String, Doctor> doctors;
+    public class DepartmentMapper {
+        public String id;
+        private Map<String, String> doctors =
+                new HashMap<String, String>();
     }
 
-    public class Doctor extends NameIdMapper {
+
+    private Map<String, HospitalMapper> hospitals;
+    private static NameIdMapper ins;
+
+    {
+        hospitals = new HashMap<String, HospitalMapper>();
+    }
+
+    private NameIdMapper() {
+    }
+
+    public static NameIdMapper getInstance() {
+        if (ins == null) {
+            ins = new NameIdMapper();
+        }
+
+        return ins;
+    }
+
+
+    public void clearHospital(String name) {
+        hospitals.remove(name);
+    }
+
+    public void clearHospitals() {
+        hospitals.clear();
+    }
+
+
+    public void addHospital(String name, String id) {
+        HospitalMapper hospitalMapper = new HospitalMapper();
+        hospitalMapper.id = id;
+        hospitals.put(name, hospitalMapper);
+    }
+
+    public void addDepartment(String hospitalName, String departmentName, String id) {
+        HospitalMapper hospitalMapper = hospitals.get(hospitalName);
+        if (hospitalMapper == null) {
+            throw new Ws320Exception("hospital not found: " + hospitalName);
+        }
+        DepartmentMapper departmentMapper = new DepartmentMapper();
+        departmentMapper.id = id;
+        hospitalMapper.departments.put(departmentName, departmentMapper);
+    }
+
+    public void addDoctor(String hospitalName, String departmentName, String doctorName, String id) {
+        HospitalMapper hospitalMapper = hospitals.get(hospitalName);
+        if (hospitalMapper == null) {
+            throw new Ws320Exception("hospital not found: " + hospitalName);
+        }
+
+        DepartmentMapper departmentMapper = hospitalMapper.departments.get(departmentName);
+        if (departmentMapper == null) {
+            throw new Ws320Exception("department not found: " + hospitalName + ", " + departmentName);
+        }
+
+        departmentMapper.doctors.put(doctorName, id);
     }
 }
