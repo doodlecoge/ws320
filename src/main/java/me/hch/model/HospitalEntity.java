1ca217ae208a8d2dbbd8e1de3eeecc7496941151
huaichao wang <hchwang001@gmail.com>
2014-05-02 20:06:20 +0800

    add hibernate ...

---
+++ src/main/java/me/hch/model/HospitalEntity.java
@@ -0,0 +1,105 @@
+package me.hch.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by hch on 2014/5/2.
+ */
+
+@Entity
+@Table(name = "hospitals")
+public class HospitalEntity implements Serializable {
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String grade;
+
+    @Column
+    private String kind;
+
+    @Column
+    private String address;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String intro;
+
+    @Column
+    private String wsdl;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public String getWsdl() {
+        return wsdl;
+    }
+
+    public void setWsdl(String wsdl) {
+        this.wsdl = wsdl;
+    }
+}
---
+++ src/main/java/me/hch/util/HibernateUtil.java
@@ -0,0 +1,72 @@
+package me.hch.util;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hch on 2014/5/2.
+ */
+public class HibernateUtil {
+
+    /**
+     * ThreadLocal Session Map
+     */
+    public static final ThreadLocal MAP = new ThreadLocal();
+
+    private static final Logger LOG = LoggerFactory.getLogger(HibernateUtil.class);
+
+    private static final SessionFactory SESSION_FACTORY;
+
+    /**
+     * Make default construct private
+     */
+    private HibernateUtil() {
+    }
+
+    /** Loads Hibernate config. */
+    static {
+        try {
+            LOG.debug("HibernateUtil.static - loading config");
+            SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
+            LOG.debug("HibernateUtil.static - end");
+        } catch (HibernateException ex) {
+            throw new RuntimeException("Exception building SessionFactory: " + ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * Gets Hibernate Session for current thread.  When finished, users
+     * must return session using {@link #closeSession() closeSession()} method.
+     *
+     * @return Hibernate Session for current thread.
+     * @throws HibernateException if there is an error opening a new session.
+     */
+    public static Session currentSession() throws HibernateException {
+        Session s = (Session) MAP.get();
+        // Open a new Session, if this Thread has none yet
+        if (s == null) {
+            s = SESSION_FACTORY.openSession();
+            MAP.set(s);
+        }
+        return s;
+    }
+
+    /**
+     * Closes the Hibernate Session.  Users must call this method after calling
+     * {@link #currentSession() currentSession()}.
+     *
+     * @throws HibernateException if session has problem closing.
+     */
+    public static void closeSession() throws HibernateException {
+        Session s = (Session) MAP.get();
+        MAP.set(null);
+        if (s != null) {
+            s.close();
+        }
+    }
+}
