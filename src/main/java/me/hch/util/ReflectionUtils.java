99be816146d7a8a01a9b41ee622cc3386be0b536
huaichao wang <hchwang001@gmail.com>
2014-05-02 16:48:29 +0800

    clone

---
+++ src/main/java/me/hch/util/ReflectionUtils.java
@@ -0,0 +1,33 @@
+package me.hch.util;
+
+import me.hch.Ws320Exception;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by hch on 2014/5/2.
+ */
+public class ReflectionUtils {
+    public static <T> T clone(Class<T> klass, Object obj) {
+        T t = null;
+
+        try {
+            t = klass.newInstance();
+        } catch (Exception e) {
+            throw new Ws320Exception(e);
+        }
+
+        Field[] declaredFields = klass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            try {
+                Object val = declaredField.get(obj);
+                declaredField.set(t, val);
+            } catch (IllegalAccessException e) {
+                throw new Ws320Exception(e);
+            }
+        }
+
+        return t;
+    }
+}
