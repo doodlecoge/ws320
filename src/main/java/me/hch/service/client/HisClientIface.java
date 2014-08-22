5c7b98d5ab8b669843bec20f50d1615f660f6d18
doodlecoge <doodlecoge@163.com>
2014-08-22 15:13:16 +0800

    remove unused imports

--- src/main/java/me/hch/service/client/HisClientIface.java
+++ src/main/java/me/hch/service/client/HisClientIface.java
@@ -1,12 +1,5 @@
 package me.hch.service.client;
 
-import me.hch.util.FileUtils;
-
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 /**
  * Created by Administrator on 14-4-13.
  */
